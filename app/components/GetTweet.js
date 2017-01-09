d3c18c1f5f3e9e196656fe598b5c9d01ab346896
smmschaefer <sms8@pdx.edu>
2017-01-09 14:36:43 -0800

    Various fixes to Twitter API call. TODO: Handle Headers

--- app/components/GetTweet.js
+++ app/components/GetTweet.js
@@ -2,42 +2,28 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import hmacsha1 from 'hmacsha1'
 
-/****
-HOW TO USE function SHA
-
-var hash = hmacsha1(KEY, DATA);
-
-***/
-
 
 const tweet = 'https://api.twitter.com/1.1/search/tweets.json?q=SamSchaeferSays';
 const myGit = 'https://api.github.com/users/smmschaefer';
 
 const consumerKey = "Zo6xzF7ncTX95t4h0NaDPANB8";
 let nonce = createNonce();;
-const signature = hmacsha1(sig);
 const signatureMethod = "HMAC-SHA1";
 const timeStamp = Date.now();
-const token = "";
+const token = "818542570833788928-szvyxgZ2OP8UsJuk3ov175Bbn9i0l7u";
 const hiddenToken ="JRdNAir4q7OUQ6uXlshAECnOIvhDnIpztjhQR1g7L4c44hyaIS";
-
 const sig = `${consumerKey}&${hiddenToken}`;
+const signature = btoa(hmacsha1(sig, "ok"));
 
-const DST = `OAuth oauth_consumer_key="${consumerKey}",
-oauth_nonce="${nonce}", 
-oauth_signature="${signature}", 
-oauth_signature_method="${signatureMethod}", 
-oauth_timestamp="${timeStamp}", 
-oauth_token="${token}", 
-oauth_version="1.0"
- `;
- 
- function createNonce() {
-	 let randMe = "thiswillbecomerandom";
+const DST = `OAuth oauth_consumer_key="${consumerKey}", oauth_nonce="${nonce}", oauth_signature="${signature}", oauth_signature_method="${signatureMethod}", oauth_timestamp="${timeStamp}", oauth_token="${token}", oauth_version="1.0"`;
+
+function createNonce() {
+	 let randMe = "";
+	 let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	     for( var i=0; i < 5; i++ )
+	         randMe += possible.charAt(Math.floor(Math.random() * possible.length));
 	 return btoa(randMe);
  }
- 
-
 
 function getUserTweet() {
     return axios.get(`${tweet}`, { headers: { 'Authorization': `${DST}` } }).then(function(response){
