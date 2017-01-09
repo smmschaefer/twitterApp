a1d3097e3654bf7381e44e87c755714e6168f7ae
smmschaefer <sms8@pdx.edu>
2017-01-09 11:10:34 -0800

    More core stuff pushed up

---
+++ app/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+
+class Main extends Component {
+	render() {
+		return(
+			<div>
+				Follow the commander-in chief through his TWEETS
+			</div>
+		)
+	}
+}
+
+
+ReactDOM.render(<Main />, document.getElementById('app'));
+
+
+
