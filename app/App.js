412a936f43c7f670eda8cfe0a3c44e05b012a817
smmschaefer <sms8@pdx.edu>
2017-01-09 13:31:29 -0800

    Setting up Twitter API connection

--- app/App.js
+++ app/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import GetTweet from './components/GetTweet'
 
 
 class Main extends Component {
 	render() {
 		return(
 			<div>
-				Follow the commander-in chief through his TWEETS
+				Follow the commander-in-chief through his TWEETS <br />
+				<GetTweet />
 			</div>
 		)
 	}
